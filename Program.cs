191c6bc4fe8f62ddce584b05d31844d8992c35f5
RD <ricardodias169@gmail.com>
2021-04-10 19:04:47 -0300

    Finalizando Programa

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Sinal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            double num;
+            Console.WriteLine("---------");
+            Console.WriteLine("--Sinal--");
+            Console.WriteLine("---------");
+            Console.Write("Digite um número:  ");
+            num = Convert.ToDouble(Console.ReadLine());
+            if(num >= 1){
+                Console.WriteLine("Positivo");
+            }
+            else if(num < 0){
+                Console.WriteLine("Negativo");
+            }
+            else if(num == 0 ){
+                Console.WriteLine("Zero");
+            }
+        }
+    }
+}
